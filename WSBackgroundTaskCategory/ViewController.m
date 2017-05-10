22f3e5ec1c287625e23cfeb66ee64e4bbe557fa5
ws <466753809@qq.com>
2017-05-10 20:59:32 +0800

    no message

--- WSBackgroundTaskCategory/ViewController.m
+++ WSBackgroundTaskCategory/ViewController.m
@@ -9,14 +9,34 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSTimer *_timer;
+    NSInteger _date;
+    
+    UILabel * _textLabel;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _date = 300;
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timerFireMethod:) userInfo:nil repeats:YES];
+    
+    _textLabel = [[UILabel alloc]initWithFrame:self.view.frame];
+    _textLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:_textLabel];
+}
+
+-(void)timerFireMethod:(NSTimer *)timer {
+    _date --;
+    if (_date == 0) {
+        [_timer invalidate];
+    }
+
+    _textLabel.text = [NSString stringWithFormat:@"%ldS", _date];
 }
 
 
